16503e72120e659d5e6055763c689cd1dd09cac7
ivanigaandriana <110620062+ivanigaandriana@users.noreply.github.com>
2024-02-19 18:25:05 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var mySwiper = new Swiper('.swiper-container', {
+      // Опції Swiper тут
+      direction: 'vertical',
+      // Додайте інші опції за потребою
+    });
+  });
