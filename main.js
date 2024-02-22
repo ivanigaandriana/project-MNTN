f6d1160adc758478be043550350b5b546055b83c
ivanigaandriana <110620062+ivanigaandriana@users.noreply.github.com>
2024-02-22 11:47:49 +0200

    include scss

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var mySwiper = new Swiper('.swiper-container', {
-      // Опції Swiper тут
-      direction: 'vertical',
-      // Додайте інші опції за потребою
-    });
-  });
+  const header = document.querySelector('.header__container');
+
+  window.onscroll = () => {
+      if (window.scrollY > 200) {
+          header.classList.add("sticky");
+      } else {
+          header.classList.remove("sticky");
+      }
+  };
+});
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const burger = document.querySelector('.burger');
+    const headerList = document.querySelector('.header__list');
+
+    burger.addEventListener('click', () => {
+      burger.classList.toggle('is-active');
+      headerList.classList.toggle('open');
+  })
+
+});
